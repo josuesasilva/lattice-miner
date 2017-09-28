b2672f5d21d649778a0bc892babfdc93402d48e0
Josué Santos <josuesasilva@gmail.com>
2017-09-28 16:51:05 -0300

    Fix BCAARs and BACARs rules

--- src/main/java/fca/core/context/triadic/TriadicAlgorithms.java
+++ src/main/java/fca/core/context/triadic/TriadicAlgorithms.java
@@ -9,6 +9,8 @@ import java.text.NumberFormat;
 import java.util.Arrays;
 import java.util.HashSet;
 import java.util.Iterator;
+import java.util.Set;
+import java.util.TreeSet;
 import java.util.Vector;
 
 public class TriadicAlgorithms {
@@ -22,114 +24,126 @@ public class TriadicAlgorithms {
 	//BCAAR
 	public static String minnerBCAAR(Vector<Rule> r) {
 		String result = GUIMessages.getString("GUI.triadicBCAAR") + "\n";
-
 		Vector<TRule> curr = new Vector<TRule>();
 		Vector<Rule> curr1 = new Vector<Rule>();
 		String[] b = null;
 		int h = 0;
 		int h4 = 0;
-
+		
 		for (int i = 0; i < r.size(); i++) {
 
 			b = tokenizer2(r.elementAt(i).getAntecedent().toString(), "\\-");
+			
+			Set<String> attrSet = new TreeSet<>();
+			Set<String> modusSet = new TreeSet<>();
+			
 			if (b.length > 0) {
-				// triMajMin(b,r.elementAt(i));
 
-				int n = b.length;
 				String Modus = "";
-				// String Modus1 ="" ;
 				String Attrib = "";
-				// String Attrib1 ="" ;
-
-				// System.out.print("Entrez une seule lettre A..Z ou a..z : ");
-				for (int f = 0; f < n; f++) {
-					String str = b[f];
-
-					int d = str.length();
-					for (int j = 0; j < d; j++) {
-						char car;
-						car = str.charAt(j);
-
-						if ((car <= 'z') && (car >= 'a')) {
-
-							Modus = Modus + Character.toString(car);
-
-						} else if ((car <= 'Z') && (car >= 'A')) {
-
-							Attrib = Attrib + Character.toString(car);
-
-						} else if ((car <= '9') && (car >= '1')) {
-							//Modus = Modus + Character.toString(car);
-							Attrib = Attrib + Character.toString(car);
+				boolean isAttr = true;
+				
+				for (String s : b) {
+					String cs = s.replaceAll("[ } { ]", "");
+				
+					if (s.contains(",")) {
+						String[] ss = cs.split(",");
+						
+						for (String is : ss) {
+							
+							if (isAttr) {
+								Attrib = Attrib + is;
+								attrSet.add(is);
+							} else {
+								Modus = Modus + is;
+								modusSet.add(is);
+							}
+							
+							isAttr = !isAttr; 
 						}
-
+					} else {
+						
+						if (isAttr) {
+							Attrib = Attrib + cs;
+							attrSet.add(cs);
+						} else {
+							Modus = Modus + cs;
+							modusSet.add(cs);
+						}
+						
+						isAttr = !isAttr;
 					}
-
 				}
 
-				if ((RuleTableModel.removeDuplicates(Modus).length()
-						* RuleTableModel.removeDuplicates(Attrib).length() == r.elementAt(i)
-						.getAntecedent().size())
-						) {
+				if ((modusSet.size() * attrSet.size() == r.elementAt(i).getAntecedent().size())) {
+					
 					curr1.add(r.elementAt(i));
-					/*System.out
-								.println(h + ": " + r.elementAt(i)
-										+ " { AL (Attributes)= "
-										+ removeDuplicates(Attrib)
-										+ ", ML (Modus)= "
-										+ removeDuplicates(Modus) + " }");
-						h++; */
+					
 					Iterator<String> iterateur = r.elementAt(i)
 							.getConsequence().iterator();
 
 					String g = "";
+					
 					while (iterateur.hasNext()) {
 
 						String[] c = tokenizer2(iterateur.next(), "\\-");
+						
+						Set<String> attr1Set = new TreeSet<>();
+						Set<String> modus1Set = new TreeSet<>();
 
 						if (c.length > 0) {
 
 							String Modus1 = "";
 							String Attrib1 = "";
-							// System.out.print("Entrez une seule lettre A..Z ou a..z : ");
-							for (int w = 0; w < c.length; w++) {
-								String str = c[w];
-
-								int o = str.length();
-								for (int y = 0; y < o; y++) {
-									char car;
-									car = str.charAt(y);
-
-									if ((car <= 'z') && (car >= 'a')) {
-
-										Modus1 = Modus1
-												+ Character.toString(car);
-
-									} else if ((car <= 'Z') && (car >= 'A')) {
-
-										Attrib1 = Attrib1
-												+ Character.toString(car);
-
-									} else if ((car <= '9') && (car >= '1')) {
-										//	Modus1 = Modus1 + Character.toString(car);
-										Attrib1 = Attrib1
-												+ Character.toString(car);
+							
+							boolean isAttr1 = true;
+							for (String s : c) {
+								String cs = s.replaceAll("[ } { ]", "");
+								
+								if (s.contains(",")) {
+									String[] ss = cs.split(",");
+									
+									for (String is : ss) {
+										
+										if (isAttr1) {
+											Attrib1 = Attrib1 + is;
+											attr1Set.add(is);
+										} else {
+											Modus1 = Modus1 + is;
+											modus1Set.add(is);
+										}
+										
+										isAttr1 = !isAttr1; 
 									}
-
+								} else {
+									
+									if (isAttr1) {
+										Attrib1 = Attrib1 + cs;
+										attr1Set.add(cs);
+									} else {
+										Modus1 = Modus1 + cs;
+										modus1Set.add(cs);
+									}
+									
+									isAttr1 = !isAttr1;
 								}
-
 							}
 
+
 							HashSet<Character> h1 = new HashSet<Character>(), h2 = new HashSet<Character>();
+							
 							for (int i1 = 0; i1 < Modus1.length(); i1++) {
 								h1.add(Modus1.charAt(i1));
 							}
+							
 							for (int i1 = 0; i1 < Modus.length(); i1++) {
 								h2.add(Modus.charAt(i1));
 							}
+							
 							h1.retainAll(h2);
 							String uk = h1.toString();
 							String[] pu = tokenizer2(uk, "\\[]");
+							
 							if (pu.length > 0) {
 								g = g + Arrays.asList(c).toString();
 							}
@@ -140,34 +154,19 @@ public class TriadicAlgorithms {
 					}
 
 					if (g.length() > 0) {
+						
 						String Attri = "";
-						String[] group = tokenizer2(g, "]");
-						String tempo = Arrays.asList(group).toString();
-						//System.out.println(tempo) ;
-						for (int w1 = 0; w1 < group.length; w1++) {
-							//	String str1 = group[w1];
-							String str1 = tempo;
-							//int o1 = str1.length();
-							int o1 = tempo.length();
-							for (int y1 = 0; y1 < o1; y1++) {
-								char car1;
-								car1 = str1.charAt(y1);
-
-								if ((car1 <= 'Z') && (car1 >= 'A')) {
-
-									if (RuleTableModel.regexOccur(str1, Character.toString(car1)) == RuleTableModel.removeDuplicates(Modus).length()) {
-										Attri = Attri + Character.toString(car1);
-									}
-
-								} else if ((car1 <= '9') && (car1 >= '1')) {
-									if (RuleTableModel.regexOccur(str1, Character.toString(car1)) == RuleTableModel.removeDuplicates(Modus).length()) {
-										Attri = Attri + Character.toString(car1);
-									}
-								}
-
+						String str = g.replace("]", "").replace("[", "").replace(" ", "");
+						
+						if (str.contains("," )) {
+							
+							String attr = str.split(",")[0];
+							
+							if (RuleTableModel.regexOccur(str, attr) == modusSet.size()) {
+								Attri = attr;
 							}
-
 						}
+						
 						if (Attri.length() > 0) {
 							h4++;
 							result += h4 + ": ( " + RuleTableModel.removeDuplicates(Attrib) + " -> " + RuleTableModel.removeDuplicates(Attri) + " ) " + RuleTableModel.removeDuplicates(Modus) + " [support = " + percentFormat(r.elementAt(i).getSupport()) + " confidence = " + percentFormat(r.elementAt(i).getConfidence()) + "]\n";
@@ -186,142 +185,140 @@ public class TriadicAlgorithms {
 		String result = GUIMessages.getString("GUI.triadicBACAR") + "\n";
 		Vector<TRule> curr1 = new Vector<TRule>();
 		Vector<Rule> curr = new Vector<Rule>();
-
 		String[] b = null;
-		int h = 0;
 		int h4 = 0;
+		
 		for (int i = 0; i < r.size(); i++) {
-
+			
 			b = tokenizer2(r.elementAt(i).getAntecedent().toString(), "\\-");
+						
+			Set<String> attrSet = new TreeSet<>();
+			Set<String> modusSet = new TreeSet<>();
+			
 			if (b.length > 0) {
-				// triMajMin(b,r.elementAt(i));
 
-				int n = b.length;
 				String Modus = "";
-				// String Modus1 ="" ;
-				String Attrib = "";
-				// String Attrib1 ="" ;
-
-				// System.out.print("Entrez une seule lettre A..Z ou a..z : ");
-				for (int f = 0; f < n; f++) {
-					String str = b[f];
-
-					int d = str.length();
-					for (int j = 0; j < d; j++) {
-						char car;
-						car = str.charAt(j);
-
-						if ((car <= 'z') && (car >= 'a')) {
-
-							Modus = Modus + Character.toString(car);
-
-						} else if ((car <= 'Z') && (car >= 'A')) {
-
-							Attrib = Attrib + Character.toString(car);
-
-						} else if ((car <= '9') && (car >= '1')) {
-							//	Modus = Modus + Character.toString(car);
-							Attrib = Attrib + Character.toString(car);
+				String Attrib = "";				
+				boolean isAttr = true;
+				
+				for (String s : b) {
+					String cs = s.replaceAll("[ } { ]", "");
+					
+					if (s.contains(",")) {
+						String[] ss = cs.split(",");
+						
+						for (String is : ss) {
+							
+							if (isAttr) {
+								Attrib = Attrib + is;
+								attrSet.add(is);
+							} else {
+								Modus = Modus + is;
+								modusSet.add(is);
+							}
+							
+							isAttr = !isAttr; 
 						}
-
+					} else {
+						
+						if (isAttr) {
+							Attrib = Attrib + cs;
+							attrSet.add(cs);
+						} else {
+							Modus = Modus + cs;
+							modusSet.add(cs);
+						}
+						
+						isAttr = !isAttr;
 					}
-
 				}
 
-				if ((RuleTableModel.removeDuplicates(Modus).length()
-						* RuleTableModel.removeDuplicates(Attrib).length() == r.elementAt(i)
-						.getAntecedent().size())
-						) {
+				if ((modusSet.size() * attrSet.size() == r.elementAt(i).getAntecedent().size())) {
+					
 					curr.add(r.elementAt(i));
-					/*	System.out
-								.println(h + ": " + r.elementAt(i)
-										+ " { AL (Attributes)= "
-										+ removeDuplicates(Attrib)
-										+ ", ML (Modus)= "
-										+ removeDuplicates(Modus) + " }");
-						h++; */
+					
 					Iterator<String> iterateur = r.elementAt(i)
 							.getConsequence().iterator();
 
 					String g = "";
+					
 					while (iterateur.hasNext()) {
 
 						String[] c = tokenizer2(iterateur.next(), "\\-");
-
+						
+						Set<String> attr1Set = new TreeSet<>();
+						Set<String> modus1Set = new TreeSet<>();
+						
 						if (c.length > 0) {
 
 							String Modus1 = "";
 							String Attrib1 = "";
-							// System.out.print("Entrez une seule lettre A..Z ou a..z : ");
-							for (int w = 0; w < c.length; w++) {
-								String str = c[w];
-
-								int o = str.length();
-								for (int y = 0; y < o; y++) {
-									char car;
-									car = str.charAt(y);
-
-									if ((car <= 'z') && (car >= 'a')) {
-
-										Modus1 = Modus1
-												+ Character.toString(car);
-
-									} else if ((car <= 'Z') && (car >= 'A')) {
-
-										Attrib1 = Attrib1
-												+ Character.toString(car);
-
-									} else if ((car <= '9') && (car >= '1')) {
-										//	Modus1 = Modus1 + Character.toString(car);
-										Attrib1 = Attrib1
-												+ Character.toString(car);
-
+							
+							boolean isAttr1 = true;
+							for (String s : c) {
+								String cs = s.replaceAll("[ } { ]", "");
+								
+								if (s.contains(",")) {
+									String[] ss = cs.split(",");
+									
+									for (String is : ss) {
+										
+										if (isAttr1) {
+											Attrib1 = Attrib1 + is;
+											attr1Set.add(is);
+										} else {
+											Modus1 = Modus1 + is;
+											modus1Set.add(is);
+										}
+										
+										isAttr1 = !isAttr1; 
 									}
-
+								} else {
+									
+									if (isAttr1) {
+										Attrib1 = Attrib1 + cs;
+										attr1Set.add(cs);
+									} else {
+										Modus1 = Modus1 + cs;
+										modus1Set.add(cs);
+									}
+									
+									isAttr1 = !isAttr1;
 								}
-
 							}
 
 							HashSet<Character> h1 = new HashSet<Character>(), h2 = new HashSet<Character>();
+							
 							for (int i1 = 0; i1 < Attrib1.length(); i1++) {
 								h1.add(Attrib1.charAt(i1));
 							}
+							
 							for (int i1 = 0; i1 < Attrib.length(); i1++) {
 								h2.add(Attrib.charAt(i1));
 							}
+							
 							h1.retainAll(h2);
 							String uk = h1.toString();
 							String[] pu = tokenizer2(uk, "\\[]");
+					
 							if (pu.length > 0) {
 								g = g + Arrays.asList(c).toString();
 							}
-							;
-
+							
 						}
 
 					}
 
 					if (g.length() > 0) {
 						String Attri = "";
-						String[] group = tokenizer2(g, "]");
-						String tempo = Arrays.asList(group).toString();
-						//System.out.println(tempo) ;
-						for (int w1 = 0; w1 < group.length; w1++) {
-							//	String str1 = group[w1];
-							String str1 = tempo;
-							//int o1 = str1.length();
-							int o1 = tempo.length();
-							for (int y1 = 0; y1 < o1; y1++) {
-								char car1;
-								car1 = str1.charAt(y1);
-
-								if ((car1 <= 'z') && (car1 >= 'a')) {
-
-									if (RuleTableModel.regexOccur(str1, Character.toString(car1)) == RuleTableModel.removeDuplicates(Attrib).length()) {
-										Attri = Attri + Character.toString(car1);
-									}
-
-								}
+						
+						String str = g.replace("]", "").replace("[", "").replace(" ", "");
+						
+						if (str.contains("," )) {
+							
+							String attr = str.split(",")[1];
+							if (RuleTableModel.regexOccur(str, attr) == attrSet.size()) {
+								Attri = attr;
 							}
 						}
 
